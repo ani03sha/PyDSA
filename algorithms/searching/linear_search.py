635e4445018a63692bf51927496a53964aead557
Anirudh Sharma <anirudh03sharma@gmail.com>
2023-10-29 21:18:08 +0530

    Updated linear search and test cases

--- algorithms/searching/linear_search.py
+++ algorithms/searching/linear_search.py
@@ -1,4 +1,16 @@
+"""
+nums - source array
+target - value to be searched
+
+Time complexity - O(N)
+Space complexity - O(1)
+"""
+
+
 def linear_search(nums, target):
+    # Check if the list is null or empty
+    if nums is None or len(nums) == 0:
+        raise Exception("List cannot be null or empty")
     # Loop through the list with every element
     for index in range(len(nums)):
         # check if the current element is equal to the target
--- tests/algorithms/searching/linear_search_test.py
+++ tests/algorithms/searching/linear_search_test.py
@@ -1,41 +1,40 @@
+import unittest
 from algorithms.searching.linear_search import linear_search
 
 
-def test_linear_search_found():
-    assert linear_search([4, 2, 7, 1, 9, 5], 7) == 2
+class TestLinearSearch(unittest.TestCase):
+    def test_linear_search(self):
+        nums = [4, 2, 7, 1, 9, 5]
+        self.assertEqual(linear_search(nums, 7), 2)
+
+        nums = None
+        with self.assertRaises(Exception):
+            linear_search(nums, 3)
+
+        with self.assertRaises(Exception):
+            linear_search([], 5)
+
+        nums = [9]
+        self.assertEqual(linear_search(nums, 9), 0)
+
+        nums = [6]
+        self.assertEqual(linear_search(nums, 9), None)
+
+        nums = [3, 7, 2, 7, 9]
+        self.assertEqual(linear_search(nums, 7), 1)
+
+        nums = list(range(1000000))
+        self.assertEqual(linear_search(nums, 999999), 999999)
+
+        nums = [-1, -5, -3, -8, -10]
+        self.assertEqual(linear_search(nums, -8), 3)
+
+        nums = ['apple', 3, 'banana', True]
+        self.assertEqual(linear_search(nums, 'banana'), 2)
+
+        nums = [1.5, 3.7, 2.8, 5.2]
+        self.assertEqual(linear_search(nums, 2.8), 2)
 
 
-def test_linear_search_not_found():
-    assert linear_search([4, 2, 7, 1, 9, 5], 3) is None
-
-
-def test_linear_search_empty_list():
-    assert linear_search([], 5) is None
-
-
-def test_linear_search_single_element_found():
-    assert linear_search([9], 9) == 0
-
-
-def test_linear_search_single_element_not_found():
-    assert linear_search([6], 9) is None
-
-
-def test_linear_search_duplicate_elements():
-    assert linear_search([3, 7, 2, 7, 9], 7) == 1
-
-
-def test_linear_search_large_list():
-    assert linear_search(list(range(1000000)), 999999) == 999999
-
-
-def test_linear_search_negative_numbers():
-    assert linear_search([-1, -5, -3, -8, -10], -8) == 3
-
-
-def test_linear_search_mixed_data_types():
-    assert linear_search(['apple', 3, 'banana', True], 'banana') == 2
-
-
-def test_linear_search_floats():
-    assert linear_search([1.5, 3.7, 2.8, 5.2], 2.8) == 2
+if __name__ == '__main__':
+    unittest.main()
